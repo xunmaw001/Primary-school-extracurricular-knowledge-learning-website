b47dfefbfae7648266d70611cfc548e4f71fabd2
yulinyu <2508000341@QQ.COM>
2025-01-27 19:25:23 +0800

    1

---
+++ src/main/webapp/admin/src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+                return {
+            url : "http://localhost:8080/ssmw8e5k/",
+            name: "ssmw8e5k",
+            // 退出到首页链接
+            indexUrl: 'http://localhost:8080/ssmw8e5k/front/index.html'
+        };
+            },
+    getProjectName(){
+        return {
+            projectName: "小学生课外知识学习网站"
+        } 
+    }
+}
+export default base
